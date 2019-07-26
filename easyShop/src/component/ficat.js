a43078a266a5fe48a1f4cf4da2f17f0ab8360317
houfaxing <houfaxing1024@163.com>
2019-07-26 10:09:24 +0800

    '历史纪录开始'

--- easyShop/src/component/ficat.js
+++ easyShop/src/component/ficat.js
@@ -3,33 +3,65 @@ import BScroll from 'better-scroll';
  class Ficat extends Component {
 	 render() {
 		let {fication,currentId,id,goods}=this.props
-    console.log(goods)
+    console.log(fication)
         return (
-			<>
-			   <span>
-			     { <>
+					<>
+						 <span>
 						 {/*渲染图片和下面标题*/}
-						    <img src={currentId.banner_url}/>
-						    <em>{currentId.front_name}</em>
-						 </>
-					 }
-			   </span>
-				
-			   <p>- {currentId.name}分类 -</p>
-			      <ol className="subCategory">
-						  {
-						  	goods&&goods.map((item,index)=>{
-						  		return <li>
-						  				<span><img src={item.list_pic_url}/></span>
-											<span>{item.name}</span>
-						  		</li>
-						  	})
-						  }
-					</ol>
-			   
-			 </>
+						 {
+							 console.log(currentId.banner_url)
+						 }
+							 { <>
+								    <img src={currentId.banner_url}/>
+										<em>{currentId.front_name}</em>
+								 </>
+							 }
+						 </span>
+						 {/* 分类*/}
+						 {this.tail(fication,currentId)}
+						 <ol className="subCategory">
+								{
+									this.goodFication(goods,fication)
+								}
+						 </ol>
+					</>
         )
     }
-		
+		//分类
+		tail(fication,currentId){
+				return fication.map((item,index)=>{
+					console.log(item)
+					 if(item.id===1005000){
+							 if(currentId.name){
+								 return <p key={index}>-  {currentId.name}分类 -</p>
+							 }else{
+								  return <p key={index}>-  {item.name}分类 -</p>
+							 }
+					 }
+				})
+		}
+	 //分类下面的
+	 goodFication(goods,fication){
+		  if(goods){
+				 return goods.map((item,index)=>{
+						return <li>
+								<span><img src={item.list_pic_url}/></span>
+								<span>{item.name}</span>
+						</li>
+					})
+			}else{
+				 return fication.map((item,index)=>{
+				 	if(item.id===1005000){
+				 			return item.goodsList.map((ite,ind)=>{
+								 return <li>
+								 		<span><img src={ite.list_pic_url}/></span>
+								 		<span>{item.name}</span>
+								 </li>
+							})
+				  	}
+				 })
+			}
+	 }
+	
 }
 export default Ficat
--- easyShop/src/viewser/goodsSearch.js
+++ easyShop/src/viewser/goodsSearch.js
@@ -1,10 +1,39 @@
 import React, { Component } from 'react'
 import Footer from '../component/footer';
+import '../sass/goodsSearch.scss';
+import { Icon } from 'antd';
 class GoodsSearch extends Component {
     render() {
         return (
-            <div className="wrap">
-            	input
+            <div className="noTabPageContent">
+            	<ul className="searchInputWrap">
+				    <li>&lt;</li>
+					<li><Icon type="search" /><input type="text" placeholder="520元礼包抢先领"/></li>
+					<li>取消</li>
+				</ul>
+				<div className="searchMsg">
+				   <div className="searchItemWrap">
+				       <ul className="title">
+					      <li>历史纪录</li>
+						  <li><Icon type="delete" /></li>
+					   </ul>
+					   <ol className="listWrap">
+					      <li>历史纪录</li>
+						  <li>历史纪录</li>
+						  <li>历史纪录</li>
+					   </ol>
+				   </div>
+				   <div className="searchItemwrap">
+				      <p className="title">热门搜索</p>
+					  <div className="listWrap">
+					     <button class="listItem">520元礼包抢先领</button>
+						 <button class="listItem">夏ss</button>
+						 <button class="listItem">单鞋</button>
+						 <button class="listItem">夏凉被</button>
+						 <button class="listItem">墨镜</button>
+					  </div>
+				   </div>
+				</div>
             </div>
         )
     }
