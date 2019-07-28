c0d1b531f8b561aea54413ae3309471fe37899e3
myl <1244282340@qq.com>
2019-07-28 19:40:24 +0800

    更多

--- easyShop/src/viewser/home.js
+++ easyShop/src/viewser/home.js
@@ -15,117 +15,113 @@ import Special from "../component/special/special"
 @observer
 class Home extends Component {
 
-    render() {
-        console.log(this.props.home)
-        return (
-            <div className="wrap">
-                <div className="header">
-                    {/* swier */}
-                    <div className="swiper">
-                        <div className="swiper-container">
-                            <div className="swiper-wrapper">
-                                {
-                                    this.props.home && this.props.home.bannerdata.map(item => (
-                                        <div className="swiper-slide" key={item.id}>
-                                            <img src={item.image_url} alt="" />
-                                        </div>
-                                    ))
+render() {
+console.log(this.props.home)
+return (
+<div className="wrap">
+    <div className="header">
+{/* swier */}
+<div className="swiper">
+    <div className="swiper-container">
+        <div className="swiper-wrapper">
+            {this.props.home && this.props.home.bannerdata.map(item => (
+                <div className="swiper-slide" key={item.id}>
+                    <img src={item.image_url} alt="" />
+                </div>
+            ))}
+        </div>
+        <div className='swiper-pagination'></div>
+    </div>
+</div>
+{/* swier下的框 */}
+<div className="content">
+    <NavTo />
+</div>
+{/* 品牌制造商 */}
 
-                                }
-                            </div>
-                            <div className='swiper-pagination'></div>
+<div className="brands">
+    <p>品牌制造商直销</p>
+    <Manufacturer />
+</div>
+{/* 新品首发 */}
+<div className="newProduct">
+    <p>新品首发</p>
+    <NewProduct />
+</div>
+{/* 人气推荐 */}
+<div className="popularity">
+    <p>人气推荐</p>
+    <Popularity />
+</div>
+{/* 专题精选 */}
+<div className="special">
+    <p>专题精选</p>
+    <Special />
+</div>
+{/* 居家   以下所有 */}
+<div className="category">
+{
+    this.props.home && this.props.home.categoryListdata.map(item => (
+    <div key={item.id}>
+        <p key={item.id}>{item.name}</p>
+        <div className="categorys">
+        {
+            item.goodsList.map(ite => (
+                // console.log()
+                <div className="cate" key={ite.id}>
+                    <NavLink className="categorysMenu"
+                        // key={item.id}
+                        to={`/Goods/${item.id}`}
+                    >
+                        <img src={ite.list_pic_url} alt="" />
+                        <div className="cateTop">
+                            <h4>{ite.name}</h4>
+                            <h5>￥{ite.retail_price}</h5>
                         </div>
-                    </div>
-                    {/* swier下的框 */}
-                    <div className="content">
-                        <NavTo />
-                    </div>
-                    {/* 品牌制造商 */}
-
-                    <div className="brands">
-                        <p>品牌制造商直销</p>
-                        <Manufacturer />
-                    </div>
-                    {/* 新品首发 */}
-                    <div className="newProduct">
-                        <p>新品首发</p>
-                        <NewProduct />
-
-                    </div>
-                    {/* 人气推荐 */}
-                    <div className="popularity">
-                        <p>人气推荐</p>
-                        <Popularity />
-                    </div>
-                    {/* 专题精选 */}
-                    <div className="special">
-                        <p>专题精选</p>
-                        <Special />
-                    </div>
-                    {/* 居家   以下所有 */}
-                    <div className="category">
-                        {
-                            this.props.home && this.props.home.categoryListdata.map(item => (
-                                <div key={item.id}>
-                                    <p key={item.id}>{item.name}</p>
-                                    <div className="categorys">
-                                    {
-                                        item.goodsList.map(ite => (
-                                            // console.log()
-                                            <div className="cate" key={ite.id}>
-                                                <NavLink className="categorysMenu"
-                                                    // key={item.id}
-                                                    to={`/Goods/${item.id}`}
-                                                >
-                                                    <img src={ite.list_pic_url} alt="" />
-                                                    <div className="cateTop">
-                                                        <h4>{ite.name}</h4>
-                                                        <h5>￥{ite.retail_price}</h5>
-                                                    </div>
-                                                </NavLink>
-                                            </div>
-
-                                        ))
-
-                                    }
-  <Link to={`/categorys/${item.id}`} className="categoryMoreGoods">
-                <div className="categ">
-                    更多{item.name}好物<br />
-                    <Icon type="right-circle" className="iconstyle"/>
+                    </NavLink>
                 </div>
-                
-              </Link>
-                                    </div>
-                                   
-                                </div>
-                            ))
 
-                        }
+            ))
 
-
-                    </div>
-                </div>
-                <Footer />
+        }
+        <Link to={`/categorys/${item.id}`} className="categoryMoreGoods">
+            <div className="categ">
+                更多{item.name}好物<br />
+                <Icon type="right-circle" className="iconstyle" />
             </div>
-        )
-    }
 
-    componentDidMount() {
-        var mySwiper = new Swiper('.swiper-container', {
-            loop: true,//这里是自动轮播
-            autoplay: {
-                delay: 1000,
-                disableOnInteraction: false
-            },
-            pagination: {
-                el: '.swiper-pagination',//这里是分页器设置
-                type: "bullets"
-            },
-            observer: true,
-            observerParents: true
-        });
-        this.props.home.gethomeData()
+        </Link>
+        </div>
 
-    }
+    </div>
+    ))
+}
+
+
+</div>
+{/* 首页完成 */}
+    </div>
+    <Footer />
+</div>
+)
+}
+
+componentDidMount() {
+    var mySwiper = new Swiper('.swiper-container', {
+        loop: true,//这里是自动轮播
+        autoplay: {
+            delay: 1000,
+            disableOnInteraction: false
+        },
+        pagination: {
+            el: '.swiper-pagination',//这里是分页器设置
+            type: "bullets"
+        },
+        observer: true,
+        observerParents: true
+    });
+    this.props.home.gethomeData()
+
+}
 }
 export default Home
